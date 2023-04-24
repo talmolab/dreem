150f515ca2e103890241177ac90363c5d19bd26a
sheridana <asheridan@salk.edu>
2023-04-24 16:12:53 -0400

    Expose modules

--- biogtr/__init__.py
+++ biogtr/__init__.py
@@ -1,3 +1,7 @@
 """Top-level package for BioGTR."""
 
 from biogtr.version import __version__
+from biogtr.models.attention_head import MLP, ATTWeightHead
+from biogtr.models.visual_encoder import VisualEncoder
+from biogtr.models.embedding import Embedding
+from biogtr.models.transformer import Transformer
