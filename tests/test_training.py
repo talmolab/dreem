bbc383bdba6103a9820726b7ee9bf132a0222c8a
sheridana <asheridan@salk.edu>
2023-04-28 13:08:20 -0400

    Remove device logic from training test

--- tests/test_training.py
+++ tests/test_training.py
@@ -4,9 +4,10 @@ from biogtr.training.losses import AssoLoss
 # todo: add named tensor tests
 # todo: add fixtures
 
+torch.set_default_device("cpu")
+
 
 def test_asso_loss():
-    device = "cpu"
     num_frames = 5
     num_detected = 20
     img_shape = (1, 128, 128)
@@ -17,13 +18,13 @@ def test_asso_loss():
         instances.append(
             {
                 "img_shape": torch.tensor(img_shape),
-                "num_detected": torch.tensor([num_detected]).to(device),
-                "gt_track_ids": torch.arange(num_detected).to(device),
-                "bboxes": torch.rand(size=(num_detected, 4)).to(device),
+                "num_detected": torch.tensor([num_detected]),
+                "gt_track_ids": torch.arange(num_detected),
+                "bboxes": torch.rand(size=(num_detected, 4)),
             }
         )
 
-    asso_loss = AssoLoss(neg_unmatched=True, asso_weight=10.0).to(device)
+    asso_loss = AssoLoss(neg_unmatched=True, asso_weight=10.0)
 
     asso_preds = torch.rand(size=(1, 100, 100))
 
