75a9d9af8b99a7614ca9338dca5f8b4cda0bacbf
sheridana <asheridan@salk.edu>
2023-05-31 18:58:23 -0400

    Add test cpu override for actions

--- tests/fixtures/torch.py
+++ tests/fixtures/torch.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import torch
 
@@ -5,11 +6,17 @@ import torch
 @pytest.fixture(autouse=True)
 def set_default_device(request):
     device = request.param if hasattr(request, "param") else None
+
     if device is None:
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        device = os.environ.get("OVERRIDE_DEVICE")
+        if device is None or device == "":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+
     torch.set_default_device(device)
+
     if device == "cuda":
         torch.set_default_tensor_type("torch.cuda.FloatTensor")
     else:
         torch.set_default_tensor_type("torch.FloatTensor")
+
     return device
