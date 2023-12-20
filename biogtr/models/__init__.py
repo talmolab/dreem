90ae3fd8c868ef6dbe68f6f1f7236228b232f1be
aaprasad <aaprasad.ucsd.edu>
2023-12-20 13:26:01 -0800

    update init, environment.yml temp add dependencies to toml for batch jobs (will change later)

--- biogtr/models/__init__.py
+++ biogtr/models/__init__.py
@@ -1,6 +1,5 @@
 """Model architectures and layers."""
 
 from .attention_head import MLP, ATTWeightHead
-from .embeddings.embedding import Embedding
 from .transformer import Transformer
 from .feature_encoders.visual_encoder import VisualEncoder
