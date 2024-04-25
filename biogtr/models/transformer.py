71cb83a1c5c502ba17e8ea0c576003b1a0008729
aaprasad <aaprasad@ucsd.edu>
2024-04-25 12:27:03 -0700

    lint

--- biogtr/models/transformer.py
+++ biogtr/models/transformer.py
@@ -60,13 +60,13 @@ class Transformer(torch.nn.Module):
             return_embedding: Whether to return the positional embeddings
             decoder_self_attn: If True, use decoder self attention.
 
-            More details on `embedding_meta`: 
+            More details on `embedding_meta`:
                 By default this will be an empty dict and indicate
                 that no positional embeddings should be used. To use the positional embeddings
                 pass in a dictionary containing a "pos" and "temp" key with subdictionaries for correct parameters ie:
                 {"pos": {'mode': 'learned', 'emb_num': 16, 'over_boxes: 'True'},
-                "temp": {'mode': 'learned', 'emb_num': 16}}. (see `biogtr.models.embeddings.Embedding.EMB_TYPES` 
-                and `biogtr.models.embeddings.Embedding.EMB_MODES` for embedding parameters). 
+                "temp": {'mode': 'learned', 'emb_num': 16}}. (see `biogtr.models.embeddings.Embedding.EMB_TYPES`
+                and `biogtr.models.embeddings.Embedding.EMB_MODES` for embedding parameters).
         """
         super().__init__()
 
