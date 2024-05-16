dad2b708b3f013ba9a2aa16b5c931bbec597db28
aaprasad <aaprasad@ucsd.edu>
2024-05-16 15:50:31 -0700

    Implement AssociationMatrix class for handling model output

--- biogtr/io/__init__.py
+++ biogtr/io/__init__.py
@@ -2,5 +2,6 @@
 
 from biogtr.io.frame import Frame
 from biogtr.io.instance import Instance
+from biogtr.io.association_matrix import AssociationMatrix
 
 # TODO: expose config without circular import error from biogtr.io.config import Config
