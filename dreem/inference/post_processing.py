- bug fix relating to missed detections in prev frame. now uses all frames in context window. Still keeps units of pixel, and euclidean dist rather than squared dist. also keeps the simplified approach without extra distance normalization

dreem/inference/post_processing.py
@@ -124,8 +124,8 @@ def filter_max_center_dist(
     asso_output: torch.Tensor,
     max_center_dist: float = 0,
     id_inds: torch.Tensor | None = None,
-    curr_frame_boxes: torch.Tensor | None = None,
-    prev_frame_boxes: torch.Tensor | None = None,
+    query_boxes_px: torch.Tensor | None = None,
+    nonquery_boxes_px: torch.Tensor | None = None,
 ) -> torch.Tensor:
     """Filter trajectory score by distances between objects across frames.
 
@@ -133,23 +133,22 @@ def filter_max_center_dist(
         asso_output: An N_t x N association matrix
         max_center_dist: The euclidean distance threshold between bboxes
         id_inds: track ids
-        curr_frame_boxes: the raw bbox coords of the current frame instances
-        prev_frame_boxes: the raw bbox coords of the previous frame instances
+        query_boxes_px: the raw bbox coords of the current frame instances
+        nonquery_boxes_px: the raw bbox coords of the instances in the nonquery frames (context window)
 
     Returns:
         An N_t x N association matrix
     """
     if max_center_dist is not None and max_center_dist > 0:
         assert (
-            curr_frame_boxes is not None
-            and prev_frame_boxes is not None
-            and id_inds is not None
-        ), "Need `curr_frame_boxes`, `prev_frame_boxes`, and `id_ind` to filter by `max_center_dist`"
+            query_boxes_px is not None
+            and nonquery_boxes_px is not None
+        ), "Need `query_boxes_px`, and `nonquery_boxes_px` to filter by `max_center_dist`"
 
-        k_ct = (curr_frame_boxes[:, :, :2] + curr_frame_boxes[:, :, 2:]) / 2
+        k_ct = (query_boxes_px[:, :, :2] + query_boxes_px[:, :, 2:]) / 2
         # k_s = ((curr_frame_boxes[:, :, 2:] - curr_frame_boxes[:, :, :2]) ** 2).sum(dim=2)  # n_k
         # nonk boxes are only from previous frame rather than entire window
-        nonk_ct = (prev_frame_boxes[:, :, :2] + prev_frame_boxes[:, :, 2:]) / 2
+        nonk_ct = (nonquery_boxes_px[:, :, :2] + nonquery_boxes_px[:, :, 2:]) / 2
 
         # pairwise euclidean distance in units of pixels
         dist = ((k_ct[:, None, :, :] - nonk_ct[None, :, :, :]) ** 2).sum(dim=-1) ** (

dreem/inference/tracker.py
@@ -345,16 +345,11 @@ class Tracker:
         query_frame.add_traj_score("asso_nonquery", asso_nonquery_df)
 
         # get raw bbox coords of prev frame instances from frame.instances_per_frame
-        prev_frame_ind = query_ind - 1
-        prev_frame_instances = frames[prev_frame_ind].instances
-        prev_frame_instance_ids = torch.cat(
-            [instance.pred_track_id for instance in prev_frame_instances], dim=0
-        )
-        prev_frame_boxes = torch.cat(
-            [instance.bbox for instance in prev_frame_instances], dim=0
-        )
-        curr_frame_boxes = torch.cat(
+        query_boxes_px = torch.cat(
             [instance.bbox for instance in query_frame.instances], dim=0
+        )
+        nonquery_boxes_px = torch.cat(
+            [instance.bbox for nonquery_frame in frames if nonquery_frame.frame_id != query_frame.frame_id for instance in nonquery_frame.instances], dim=0
         )
 
         pred_boxes = model_utils.get_boxes(all_instances)
@@ -369,10 +364,6 @@ class Tracker:
         id_inds = (
             unique_ids[None, :] == instance_ids[:, None]
         ).float()  # (n_nonquery, n_traj)
-
-        prev_frame_id_inds = (
-            unique_ids[None, :] == prev_frame_instance_ids[:, None]
-        ).float()  # (n_prev_frame_instances, n_traj)
 
         ################################################################################
 
@@ -445,9 +436,9 @@ class Tracker:
         traj_score = post_processing.filter_max_center_dist(
             traj_score,
             self.max_center_dist,
-            prev_frame_id_inds,
-            curr_frame_boxes,
-            prev_frame_boxes,
+            id_inds,
+            query_boxes_px,
+            nonquery_boxes_px,
         )
 
         if self.max_center_dist is not None and self.max_center_dist > 0: