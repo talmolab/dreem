- fix metrics calculation to account for track_id=-1 for low conf preds - add user param in config for setting thresh - complete implementation

dreem/inference/tracker.py
@@ -32,6 +32,7 @@ class Tracker:
         max_gap: int = inf,
         max_tracks: int = inf,
         verbose: bool = False,
+        uncertainty_threshold: float = 0.2,
         **kwargs,
     ):
         """Initialize a tracker to run inference.
@@ -50,6 +51,7 @@ class Tracker:
             max_tracks: the maximum number of tracks that can be created while tracking.
                 We force the tracker to assign instances to a track instead of creating a new track if max_tracks has been reached.
             verbose: Whether or not to turn on debug printing after each operation.
+            uncertainty_threshold: threshold for filtering out instances with high uncertainty.
             **kwargs: Additional keyword arguments (unused but accepted for compatibility).
         """
         self.track_queue = TrackQueue(
@@ -64,6 +66,7 @@ class Tracker:
         self.persistent_tracking = persistent_tracking
         self.verbose = verbose
         self.max_tracks = max_tracks
+        self.uncertainty_threshold = uncertainty_threshold
 
     def __call__(
         self, model: GlobalTrackingTransformer, frames: list[Frame]
@@ -466,7 +469,7 @@ class Tracker:
         # Compute entropy for each row and filter out rows with high entropy
         entropy = -torch.sum(scaled_traj_score * torch.log(scaled_traj_score + 1e-12), axis=1)
         # remove these rows from the cost matrix, but careful to maintain indexes of the results
-        remove = entropy > 0.57
+        remove = entropy > self.uncertainty_threshold 
 
         if (remove.sum() == traj_score.shape[0]).item():
             logger.debug(f"All instances have high entropy in frame {query_frame.frame_id.item()}, skipping assignment")