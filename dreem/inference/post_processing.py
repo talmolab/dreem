- bug fix in principal axis computation in case of orientation prompt; added normalization to compare it to PCA eigvecs

dreem/inference/post_processing.py
@@ -49,7 +49,12 @@ def _compute_principal_axis_with_prompts(instance, orientation_prompt):
         elif node_name == orientation_prompt[1]:
             tip = node_coords
     if head is not None and tip is not None and ~torch.isnan(torch.tensor(head)).any() and ~torch.isnan(torch.tensor(tip)).any():
-        return torch.tensor(head - tip)
+        vec = torch.tensor(head - tip)
+        # Normalize to unit vector to compare to PCA derived principal axis
+        norm = torch.norm(vec)
+        if norm > 1e-8:
+            vec = vec / norm
+        return vec
     return None
 
 def get_principal_axis_with_fallback(instance, orientation_prompt: list[str] | None, logger, frame_id):

dreem/inference/tracker.py
@@ -415,16 +415,19 @@ class Tracker:
             
             query_principal_axes = []
             last_principal_axes = []
+            fallbacks = []
             for instance in query_poses:
                 instance_principal_axes, fallback = post_processing.get_principal_axis_with_fallback(instance, self.orientation_prompt, logger, query_frame.frame_id.item())
+                fallbacks.append(fallback)
                 query_principal_axes.append(instance_principal_axes)
             query_principal_axes = torch.stack(query_principal_axes) # (n_query, 2)
             
             for instance in last_poses:
                 instance_principal_axes, fallback = post_processing.get_principal_axis_with_fallback(instance, self.orientation_prompt, logger, query_frame.frame_id.item())
+                fallbacks.append(fallback)
                 last_principal_axes.append(instance_principal_axes)
             last_principal_axes = torch.stack(last_principal_axes) # (n_traj, 2)
-            
+            fallback = any(fallbacks)
             if fallback:
                 logger.debug(f"Orientation prompt nodes not visible for some instances in frame {query_frame.frame_id.item()}. Used all available nodes to compute orientation of instance")
 