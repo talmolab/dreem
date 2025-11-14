- important bug fix with broadcasting in max center dist in case where more query instances than nonquery - important bug fix where track queues were not being cleared in case of multiple test files being run from a single directory - add new unit tests to capture the shape test in max center dist

dreem/inference/post_processing.py
@@ -322,7 +322,7 @@ def filter_max_center_dist(
     dist = ((k_ct[:, None, :, :] - nonk_ct[None, :, :, :]) ** 2).sum(dim=-1) ** (
         1 / 2
     )  # n_k x n_nonk
-    dist = dist.squeeze() / diag_length  # n_k x n_nonk
+    dist = dist.squeeze(-1) / diag_length  # n_k x n_nonk
     while dist.dim() < 2:
         dist = dist.unsqueeze(0)
     asso_scale = asso_output.mean(dim=1)